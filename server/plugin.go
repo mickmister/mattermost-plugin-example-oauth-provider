fe1ea0b81ba45ef0456f17a151e74c63c8556d28
mickmister <6913320+mickmister@users.noreply.github.com>
2024-01-25 14:06:34 -0500

    handles /token /command and /webhook routes

--- server/plugin.go
+++ server/plugin.go
@@ -1,28 +1,93 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"sync"
 
+	"github.com/mattermost/mattermost/server/public/model"
 	"github.com/mattermost/mattermost/server/public/plugin"
 )
 
-// Plugin implements the interface expected by the Mattermost server to communicate between the server and plugin processes.
 type Plugin struct {
 	plugin.MattermostPlugin
 
-	// configurationLock synchronizes access to the configuration.
 	configurationLock sync.RWMutex
 
-	// configuration is the active plugin configuration. Consult getConfiguration and
-	// setConfiguration for usage.
 	configuration *configuration
 }
 
-// ServeHTTP demonstrates a plugin that handles HTTP requests by greeting the world.
+type TokenResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+}
+
 func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
+	switch r.URL.Path {
+	case "/token":
+		p.handleTokenRequest(w, r)
+	case "/command":
+		p.handleCommandRequest(w, r)
+	case "/webhook":
+		p.handleWebhookRequest(w, r)
+	default:
+		handleNotFound(w, r)
+	}
+}
+
+func (p *Plugin) handleWebhookRequest(w http.ResponseWriter, r *http.Request) {
+	webhookResponse := model.OutgoingWebhookResponse{
+		Text: model.NewString("called the webhook"),
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(webhookResponse)
+	if err != nil {
+		p.API.LogError("failed to marshal webhook response", "err", err.Error())
+	}
+}
+
+func (p *Plugin) handleCommandRequest(w http.ResponseWriter, r *http.Request) {
+	authHeader := r.Header.Get("Authorization")
+	parts := strings.Split(authHeader, " ")
+	tokenType, accessToken := parts[0], parts[1]
+
+	msg := fmt.Sprintf("token type: '%s' access token: '%s'", tokenType, accessToken)
+
+	// TODO make a plugin setting and verify token matches here
+
+	commandResponse := model.CommandResponse{
+		Text:         msg,
+		ResponseType: model.CommandResponseTypeEphemeral,
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(commandResponse)
+	if err != nil {
+		p.API.LogError("failed to marshal command response", "err", err.Error())
+	}
+}
+
+func (p *Plugin) handleTokenRequest(w http.ResponseWriter, r *http.Request) {
+	conf := p.getConfiguration()
+
+	tokenResponse := TokenResponse{
+		AccessToken: conf.AccessToken,
+		TokenType:   "Bearer",
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(tokenResponse)
+	if err != nil {
+		p.API.LogError("failed to marshal token response", "err", err.Error())
+	}
+}
+
+func handleNotFound(w http.ResponseWriter, r *http.Request) {
+	msg := fmt.Sprintf("OAuth Example plugin does not handle the path `%s`", r.URL.Path)
+	http.Error(w, msg, http.StatusNotFound)
 }
 
 // See https://developers.mattermost.com/extend/plugins/server/reference/
